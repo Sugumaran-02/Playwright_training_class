e804770554134c7264c2835f2df28cb6556e09b2
Sugumaran <suguprasanth79285@gmail.com>
2025-04-14 13:56:44 +0530

    Update groping.spec.js

--- tests/groping.spec.js
+++ tests/groping.spec.js
@@ -25,8 +25,8 @@ test.describe('Group2', ()=>{
                 console.log("The test5");
                 })
 
-               test('test06', async({page}) =>{
-                console.log("The test06");
+               test('test6', async({page}) =>{
+                console.log("The test6");
                 })
 })
 
